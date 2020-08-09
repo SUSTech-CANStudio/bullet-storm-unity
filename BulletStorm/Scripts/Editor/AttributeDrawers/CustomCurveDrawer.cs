2b92c240060e7ed9a64ab3fa86aa399defc97c6f
ifact <xyyjxl@outlook.com>
2020-08-09 13:02:18 +0800

    Adjust file structure

---
+++ BulletStorm/Scripts/Editor/AttributeDrawers/CustomCurveDrawer.cs
@@ -0,0 +1,17 @@
+﻿using BulletStorm.Util.EditorAttributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace BulletStorm.Editor.AttributeDrawers
+{
+    [CustomPropertyDrawer(typeof(CustomCurveAttribute))]
+    internal class CustomCurveDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            if (attribute is CustomCurveAttribute custom)
+                EditorGUI.CurveField(position, property, custom.color, custom.range);
+        }
+    }
+}
