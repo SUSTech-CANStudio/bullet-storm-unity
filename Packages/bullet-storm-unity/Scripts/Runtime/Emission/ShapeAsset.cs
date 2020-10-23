f5944144c185913c7a589caee72c2cee26d36642
IF-ACT <xyyjxl@outlook.com>
2020-10-23 20:42:07 +0800

    build: add XNode dependency

--- Packages/bullet-storm-unity/Scripts/Runtime/Emission/ShapeAsset.cs
+++ Packages/bullet-storm-unity/Scripts/Runtime/Emission/ShapeAsset.cs
@@ -1,13 +1,14 @@
-﻿using UnityEditor;
-using UnityEngine;
-#if UNITY_EDITOR
+﻿using UnityEngine;
+using XNode;
 
+#if UNITY_EDITOR
+using UnityEditor;
 #endif
 
 namespace CANStudio.BulletStorm.Emission
 {
     [CreateAssetMenu(menuName = "BulletStorm/ShapeAsset")]
-    public class ShapeAsset : ScriptableObject
+    public class ShapeAsset : NodeGraph
     {
         public Shape shape;
 
