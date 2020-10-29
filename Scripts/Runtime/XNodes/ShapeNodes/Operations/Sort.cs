d39ecac60628fc068185b8255179dad31c591983
IF-ACT <xyyjxl@outlook.com>
2020-10-30 01:44:17 +0800

    complete sort method

--- Scripts/Runtime/XNodes/ShapeNodes/Operations/Sort.cs
+++ Scripts/Runtime/XNodes/ShapeNodes/Operations/Sort.cs
@@ -1,16 +1,20 @@
+using System.Collections.Generic;
 using CANStudio.BulletStorm.Emission;
+using UnityEngine;
+
+#pragma warning disable 0649
 
 namespace CANStudio.BulletStorm.XNodes.ShapeNodes.Operations
 {
     [CreateNodeMenu("BulletStorm/Shape/Operation/Sort", -1000), NodeTint(Utils.ColorShapeOperationSpecial)]
     public class Sort : ShapeOperationNode
     {
-        [Input(ShowBackingValue.Never, ConnectionType.Override, TypeConstraint.Inherited)]
-        public ParamComparer comparer;
+        [SerializeField]
+        private List<ParamComparer.CompareInfo> comparers;
 
         public override void Generate()
         {
-            SetShape(CopyInputShape().Sort(GetInputValue(nameof(comparer), comparer)));
+            SetShape(CopyInputShape().Sort(new ParamComparer(comparers)));
         }
     }
 }
