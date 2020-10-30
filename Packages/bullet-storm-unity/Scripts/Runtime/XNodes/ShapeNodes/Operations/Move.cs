3a9370493ad4f3aa566fe577c5fc46b66a1f7a09
IF-ACT <xyyjxl@outlook.com>
2020-10-30 15:55:07 +0800

    add more nodes

--- Packages/bullet-storm-unity/Scripts/Runtime/XNodes/ShapeNodes/Operations/Move.cs
+++ Packages/bullet-storm-unity/Scripts/Runtime/XNodes/ShapeNodes/Operations/Move.cs
@@ -5,9 +5,16 @@ namespace CANStudio.BulletStorm.XNodes.ShapeNodes.Operations
     [CreateNodeMenu("BulletStorm/Shape/Operation/Move"), NodeTint(Utils.ColorShapeOperation)]
     public class Move : ShapeOperationNode
     {
+        [Tooltip("Move all bullets by offset.")]
         [Input(connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Inherited)]
         public Vector3 offset;
+
+        [Tooltip("Move every bullets with its velocity by time.")]
+        [Input(connectionType = ConnectionType.Override, typeConstraint = TypeConstraint.Inherited)]
+        public float time;
         
-        public override void Generate() => SetShape(CopyInputShape().Move(GetInputValue(nameof(offset), offset)));
+        public override void Generate() => SetShape(CopyInputShape()
+            .Move(GetInputValue(nameof(offset), offset))
+            .Move(GetInputValue(nameof(time), time)));
     }
 }
