54df6c18899c1486e3321b14a80a7b5004943df1
IF-ACT <xyyjxl@outlook.com>
2020-11-29 00:47:13 +0800

    feat: show total time in auto shape emitter

--- Scripts/Runtime/Emitters/AutoShapeEmitter.cs
+++ Scripts/Runtime/Emitters/AutoShapeEmitter.cs
@@ -29,11 +29,14 @@ namespace CANStudio.BulletStorm.Emitters
         [Tooltip("A list where you can config every emission here."), ReorderableList, SerializeField]
         private ShapeConfig[] emissions;
 
+        [ShowNativeProperty]
+        private float TotalTime => emissions?.Aggregate(0f, (f, config) => f + config.TotalTime) ?? 0;
+
         #region reflection use only
 
         // ReSharper disable once UnusedMember.Local
         private float EmitTimes =>
-            emissions?.Aggregate(0, (i, config) => i + (config.repeat ? config.repeatTimes : 1)) ?? 0;
+            emissions?.Aggregate(0, (i, config) => i + config.RepeatTimes) ?? 0;
 
         #endregion
         
@@ -122,6 +125,23 @@ namespace CANStudio.BulletStorm.Emitters
                     if (overrideColor) copy.SetColor(color);
                     if (overrideSize) copy.SetSize(size);
                     return copy;
+                }
+            }
+
+            public int RepeatTimes => repeat ? repeatTimes : 1;
+
+            public float TotalTime
+            {
+                get
+                {
+                    var time = 0f;
+                    if (oneByOne && shape)
+                    {
+                        time += shape.shape.Count > 0 ? (shape.shape.Count - 1) * interval : 0;
+                    }
+                    time += wait;
+                    time *= RepeatTimes;
+                    return time;
                 }
             }
         }
